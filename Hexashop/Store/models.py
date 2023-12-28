f8b9a4a570c8fbdaebd444bb4f7c29aa67e96aa7
mradebowale <hamzwalex@gmail.com>
2023-12-28 20:50:55 +0100

    update

--- Hexashop/Store/models.py
+++ Hexashop/Store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -28,3 +29,39 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Payment(models.Model):
+    amount = models.PositiveIntegerField()
+    ref = models.CharField(max_length = 100)
+    email = models.EmailField()
+    verified = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    def __str__(self):
+        return f"payment {self.amount}"
+    def amount_value(self):
+        return self.amount * 100
+    
+    def save(self, *args, **kwargs):
+        while not self.ref:
+            ref = secrets.token_urlsafe(50)
+            if not Payment.objects.filter(ref=ref).exists():
+                self.ref = ref
+        super().save(*args, **kwargs)
+
+    def verify_payment(self):
+        payment = PayStack()
+        status, result = payment.verify_payment(self.ref, self.amount)
+        if status:
+            if result['amount'] / 100 == self.amount:
+                self.verified = True
+            self.save()
+        if self.verified:
+            return True
+        return False
+    
